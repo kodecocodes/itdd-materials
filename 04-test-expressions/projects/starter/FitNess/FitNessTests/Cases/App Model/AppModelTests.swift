5bed3041f556e76c36877737e4e2ffaca99a55d5
Michael Katz <mikekatz@users.noreply.github.com>
2021-07-10 10:43:20 -0400

    start with ch3 challenge

--- 04-test-expressions/projects/starter/FitNess/FitNessTests/Cases/App Model/AppModelTests.swift
+++ 04-test-expressions/projects/starter/FitNess/FitNessTests/Cases/App Model/AppModelTests.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2019 Razeware LLC
-///
+/// Copyright (c) 2021 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,6 +17,10 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
 ///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -30,36 +34,30 @@ import XCTest
 @testable import FitNess
 
 class AppModelTests: XCTestCase {
-
+  // swiftlint:disable implicitly_unwrapped_optional
   var sut: AppModel!
 
-  override func setUp() {
-    super.setUp()
+  override func setUpWithError() throws {
+    try super.setUpWithError()
     sut = AppModel()
   }
 
-  override func tearDown() {
+  override func tearDownWithError() throws {
     sut = nil
-    super.tearDown()
+    try super.tearDownWithError()
   }
-
-  // MARK: - Given
-
-  // MARK: - Lifecycle
 
   func testAppModel_whenInitialized_isInNotStartedState() {
     let initialState = sut.appState
     XCTAssertEqual(initialState, AppState.notStarted)
   }
 
-  // MARK: - Start
-  
   func testAppModel_whenStarted_isInInProgressState() {
     // when started
     sut.start()
 
     // then it is in inProgress
-    let newState = sut.appState
-    XCTAssertEqual(newState, AppState.inProgress)
+    let observedState = sut.appState
+    XCTAssertEqual(observedState, AppState.inProgress)
   }
 }
