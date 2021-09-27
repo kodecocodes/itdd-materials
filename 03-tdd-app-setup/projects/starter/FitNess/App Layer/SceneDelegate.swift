404ea5961da094b9c0e34a7b4db4f83ba7648c02
Michael Katz <mikekatz@users.noreply.github.com>
2021-09-26 20:22:46 -0400

    cleanup starter

--- 03-tdd-app-setup/projects/starter/FitNess/App Layer/SceneDelegate.swift
+++ 03-tdd-app-setup/projects/starter/FitNess/App Layer/SceneDelegate.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2019 Razeware LLC
-///
+/// Copyright (c) 2021 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,6 +17,10 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
 ///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -26,47 +30,34 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import Foundation
 import UIKit
 
-extension AppState {
-  var nextStateButtonLabel: String {
-    switch self {
-    case .notStarted:
-      return "Start"
-    case .inProgress:
-      return "Pause"
-    case .paused:
-      return "Resume"
-    case .caught:
-      return "Try Again"
-    case .completed:
-      return "Start Over"
+<<<<<<< HEAD:03-tdd-app-setup/projects/starter/FitNess/FitNess/AppDelegate.swift
+<<<<<<< HEAD:03-tdd-app-setup/projects/challenge/FitNess/FitNess/App Layer/AppMain.swift
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD:06-dependency-injection-and-mocks/projects/challenge/FitNess/FitNess/App Layer/SceneDelegate.swift
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+=======
+>>>>>>> d968637... reset for not swiftui
+@main
+struct AppMain: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
   }
-}
-
-class StepCountController: UIViewController {
-
-  @IBOutlet weak var stepCountLabel: UILabel!
-  @IBOutlet var startButton: UIButton!
-  @IBOutlet weak var chaseView: ChaseView!
-
-  init() {
-    // this is a cheat to simplify chapter 3, a proper way of getting an instance will be handled in chapter 4
-    super.init(nibName: nil, bundle: nil)
-    startButton = UIButton()
-  }
-
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-  }
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-
-  @IBAction func startStopPause(_ sender: Any?) {
-    
-  }
+<<<<<<< HEAD
+=======
+>>>>>>> d968637... reset for not swiftui:03-tdd-app-setup/projects/challenge/FitNess/FitNess/App Layer/AppMain.swift
+>>>>>>> d968637... reset for not swiftui
+=======
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+=======
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+>>>>>>> af2a68d... cleanup starter:03-tdd-app-setup/projects/starter/FitNess/App Layer/SceneDelegate.swift
+  var window: UIWindow?
+>>>>>>> 68f17d3... Revert "reset for not swiftui":03-tdd-app-setup/projects/starter/FitNess/FitNess/AppDelegate.swift
 }
--- 03-tdd-app-setup/projects/starter/FitNess/App Model/AppModel.swift
+++ 03-tdd-app-setup/projects/starter/FitNess/App Model/AppModel.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2019 Razeware LLC
-/// 
+/// Copyright (c) 2021 Razeware LLC
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,11 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,31 +30,10 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import UIKit
+import Foundation
 
-<<<<<<< HEAD:03-tdd-app-setup/projects/challenge/FitNess/FitNess/App Layer/AppMain.swift
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD:06-dependency-injection-and-mocks/projects/challenge/FitNess/FitNess/App Layer/SceneDelegate.swift
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
-=======
->>>>>>> d968637... reset for not swiftui
-@main
-struct AppMain: App {
-  var body: some Scene {
-    WindowGroup {
-      ContentView()
-    }
-  }
-<<<<<<< HEAD
-=======
->>>>>>> d968637... reset for not swiftui:03-tdd-app-setup/projects/challenge/FitNess/FitNess/App Layer/AppMain.swift
->>>>>>> d968637... reset for not swiftui
-=======
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
->>>>>>> 68f17d3... Revert "reset for not swiftui":03-tdd-app-setup/projects/starter/FitNess/FitNess/AppDelegate.swift
+public class AppModel {
+  static let instance = AppModel()
+
+  public init() {}
 }
-
