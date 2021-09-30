7ebea66f03a8b309c77a5f4a9fe0ca863897231a
Michael Katz <mikekatz@users.noreply.github.com>
2021-09-29 20:48:00 -0400

    through final

--- 03-tdd-app-setup/projects/final/FitNess/FitNess/UI Layer/ChaseView.swift
+++ 03-tdd-app-setup/projects/final/FitNess/FitNess/UI Layer/ChaseView.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2019 Razeware LLC
-///
+/// Copyright (c) 2021 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,6 +17,10 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
 ///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -26,12 +30,10 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-
-import Foundation
 import UIKit
 
-@IBDesignable class ChaseView: UIView {
-
+@IBDesignable
+class ChaseView: UIView {
   let nessieView = UIImageView()
   let runnerView = UIImageView()
 
@@ -58,6 +60,8 @@ import UIKit
   }
 
   override func prepareForInterfaceBuilder() {
+    super.prepareForInterfaceBuilder()
+
     let bundle = Bundle(for: ChaseView.self)
     nessieView.image = UIImage(named: "Nessie", in: bundle, compatibleWith: nil)
     runnerView.image = UIImage(named: "Runner", in: bundle, compatibleWith: nil)
@@ -79,7 +83,7 @@ extension AppState {
     case .caught:
       imageName = "NessieWon"
     }
-    return UIImage(named: imageName)!
+    return UIImage(imageLiteralResourceName: imageName)
   }
 
   var runnerImage: UIImage {
@@ -96,6 +100,6 @@ extension AppState {
     case .caught:
       imageName = "RunnerEaten"
     }
-    return UIImage(named: imageName)!
+    return UIImage(imageLiteralResourceName: imageName)
   }
 }
