aee18266488f58d0c1576a1e9c53280b2fbdc79b
Michael Katz <mikekatz@users.noreply.github.com>
2021-10-05 20:41:28 -0400

    update from chapter 4

--- 05-test-expectations/projects/starter/FitNess/FitNess/App Layer/SceneDelegate.swift
+++ 05-test-expectations/projects/starter/FitNess/FitNess/App Layer/SceneDelegate.swift
@@ -1,4 +1,4 @@
-/// Copyright (c) 2019 Razeware LLC
+/// Copyright (c) 2021 Razeware LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
@@ -18,6 +18,10 @@
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
 ///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,19 +30,8 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import Foundation
+import UIKit
 
-struct AlertNotification {
-  static let name = Notification.Name("Alert")
-
-  enum Keys: String {
-    case alert
-    case statusChange
-  }
-
-  enum AlertChangeStatus {
-    case added, cleared
-  }
-
-  private init() {}
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
 }
--- 05-test-expectations/projects/starter/FitNess/FitNessTests/Cases/App Model/AppModelTests.swift
+++ 05-test-expectations/projects/starter/FitNess/FitNessTests/Cases/App Model/AppModelTests.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2019 Razeware LLC
-///
+/// Copyright (c) 2021 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,6 +17,10 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
 ///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -30,17 +34,17 @@ import XCTest
 @testable import FitNess
 
 class AppModelTests: XCTestCase {
-
+  //swiftlint:disable implicitly_unwrapped_optional
   var sut: AppModel!
 
-  override func setUp() {
-    super.setUp()
+  override func setUpWithError() throws {
+    try super.setUpWithError()
     sut = AppModel()
   }
 
-  override func tearDown() {
+  override func tearDownWithError() throws {
     sut = nil
-    super.tearDown()
+    try super.tearDownWithError()
   }
 
   // MARK: - Given
@@ -50,6 +54,7 @@ class AppModelTests: XCTestCase {
 
   func givenInProgress() {
     givenGoalSet()
+    //swiftlint:disable force_try
     try! sut.start()
   }
 
@@ -68,18 +73,6 @@ class AppModelTests: XCTestCase {
   }
 
   // MARK: - Start
-    func testAppModel_whenStarted_isInInProgressState() {
-    // given
-    givenGoalSet()
-    
-    // when started
-    try? sut.start()
-
-    // then it is in inProgress
-    let newState = sut.appState
-    XCTAssertEqual(newState, AppState.inProgress)
-  }
-
   func testModelWithNoGoal_whenStarted_throwsError() {
     XCTAssertThrowsError(try sut.start())
   }
@@ -90,6 +83,18 @@ class AppModelTests: XCTestCase {
 
     // then
     XCTAssertNoThrow(try sut.start())
+  }
+
+  func testAppModel_whenStarted_isInInProgressState() {
+    // given
+    givenGoalSet()
+
+    // when started
+    try? sut.start()
+
+    // then it is in inProgress
+    let observedState = sut.appState
+    XCTAssertEqual(observedState, .inProgress)
   }
 
   // MARK: - Pause
@@ -105,6 +110,7 @@ class AppModelTests: XCTestCase {
   }
 
   // MARK: - Terminal States
+
   func testModel_whenCompleted_isInCompletedState() {
     // given
     givenCompleteReady()
@@ -152,7 +158,7 @@ class AppModelTests: XCTestCase {
   }
 
   // MARK: - Restart
-    func testAppModel_whenReset_isInNotStartedState() {
+  func testAppModel_whenReset_isInNotStartedState() {
     // given
     givenInProgress()
 
