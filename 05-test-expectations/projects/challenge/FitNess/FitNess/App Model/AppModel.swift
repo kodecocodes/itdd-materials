bf37fbb010399ae6294ffaaaccdd02cf14755eae
Michael Katz <mikekatz@users.noreply.github.com>
2021-10-07 17:52:02 -0400

    update challenge

--- 05-test-expectations/projects/challenge/FitNess/FitNess/App Model/AppModel.swift
+++ 05-test-expectations/projects/challenge/FitNess/FitNess/App Model/AppModel.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2019 Razeware LLC
-///
+/// Copyright (c) 2021 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,6 +17,10 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
 ///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -29,9 +33,7 @@
 import Foundation
 
 class AppModel {
-
   static let instance = AppModel()
-
   let dataModel = DataModel()
 
   private(set) var appState: AppState = .notStarted {
@@ -39,14 +41,14 @@ class AppModel {
       stateChangedCallback?(self)
     }
   }
-
-  var stateChangedCallback: ((AppModel) -> ())?
+  var stateChangedCallback: ((AppModel) -> Void)?
 
   // MARK: - App Lifecycle
   func start() throws {
     guard dataModel.goal != nil else {
       throw AppError.goalNotSet
     }
+
     appState = .inProgress
   }
 
@@ -63,7 +65,7 @@ class AppModel {
     guard dataModel.caught else {
       throw AppError.invalidState
     }
-    
+
     appState = .caught
   }
 
@@ -75,4 +77,3 @@ class AppModel {
     appState = .completed
   }
 }
-
--- 05-test-expectations/projects/challenge/FitNess/FitNess/UI Layer/StepCountController.swift
+++ 05-test-expectations/projects/challenge/FitNess/FitNess/UI Layer/StepCountController.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2019 Razeware LLC
-///
+/// Copyright (c) 2021 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,6 +17,10 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
 ///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -53,13 +57,13 @@ class StepCountController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    updateUI()
-
     AppModel.instance.stateChangedCallback = { model in
       DispatchQueue.main.async {
         self.updateUI()
       }
     }
+
+    updateUI()
   }
 
   func updateUI() {
@@ -80,11 +84,11 @@ class StepCountController: UIViewController {
     case .inProgress:
       AppModel.instance.pause()
     case .paused:
-      start() 
+      start()
     case .completed, .caught:
       AppModel.instance.restart()
     }
-    
+
     updateUI()
   }
 
@@ -95,13 +99,14 @@ class StepCountController: UIViewController {
       showNeedGoalAlert()
     }
   }
-  
+
   @IBAction func showSettings(_ sender: Any) {
     getGoalFromUser()
   }
 }
 
 // MARK: - Goal
+
 extension StepCountController {
   func updateGoal(newGoal: Int) {
     AppModel.instance.dataModel.goal = newGoal
@@ -110,7 +115,7 @@ extension StepCountController {
   private func showNeedGoalAlert() {
     let alertController = UIAlertController(title: "Set a goal to start", message: nil, preferredStyle: .alert)
     let cancel = UIAlertAction(title: "Cancel", style: .cancel)
-    let enterGoal = UIAlertAction(title: "Enter Goal", style: .default) { [weak self] action in
+    let enterGoal = UIAlertAction(title: "Enter Goal", style: .default) { [weak self] _ in
       self?.getGoalFromUser()
     }
 
@@ -125,10 +130,11 @@ extension StepCountController {
       textField.placeholder = "1000"
       textField.keyboardType = .numberPad
     }
-    let action = UIAlertAction(title: "Done", style: .default) { [weak self] action in
+    let action = UIAlertAction(title: "Done", style: .default) { [weak self] _ in
       guard let textField = alertController.textFields?.first else { return }
-      if let numberString = textField.text, let goal = Int(numberString) {
-        self?.updateGoal(newGoal: goal)
+      if let numberString = textField.text,
+        let goal = Int(numberString) {
+          self?.updateGoal(newGoal: goal)
       } else {
         self?.updateGoal(newGoal: 0)
       }
